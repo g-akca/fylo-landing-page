ef634cf28fa6491d8eb84adfa2512942590f4c48
g-akca <gok.akca66@gmail.com>
2026-04-19 23:01:37 +0300

    Create main components, add App styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
+    <div className="text-base leading-base min-h-screen bg-navy-850">
+      <div>
+        <Header />
+        <MainSection />
+      </div>
+      
+      <Footer />
     </div>
   )
 }
