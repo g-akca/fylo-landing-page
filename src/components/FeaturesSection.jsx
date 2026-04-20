1ad58c7f9ed68c263d4b0cfa6dc7005faca50381
g-akca <gok.akca66@gmail.com>
2026-04-20 21:43:49 +0300

    Add FeaturesSection styles, create features list

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -1,7 +1,30 @@
 function FeaturesSection() {
+  const features = [
+    {
+      imgUrl: "/images/icon-access-anywhere.svg",
+      title: "Access your files, anywhere",
+      description: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
+    },
+    {
+      imgUrl: "/images/icon-security.svg",
+      title: "Security you can trust",
+      description: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
+    },
+    {
+      imgUrl: "/images/icon-collaboration.svg",
+      title: "Real-time collaboration",
+      description: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required."
+    },
+    {
+      imgUrl: "/images/icon-any-file.svg",
+      title: "Store any type of file",
+      description: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+    }
+  ];
+
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-20">
+    </div>
   )
 }
 
