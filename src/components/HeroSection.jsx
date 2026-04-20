917efb97f5174f5f4cac2b65199643b3d89091e3
g-akca <gok.akca66@gmail.com>
2026-04-20 21:20:22 +0300

    Add HeroSection styles

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,17 +3,25 @@ import introImg from "/images/illustration-intro.png";
 function HeroSection() {
   return (
     <section className="px-1.25 flex flex-col gap-18 items-center">
-      <img src={introImg} alt="Intro illustration" className="w-[95%]" />
+      <img src={introImg} alt="Intro illustration" className="px-2.75" />
 
-      <div className="text-center">
-        <h2>All your files in one secure location, accessible anywhere.</h2>
+      <div className="text-center flex flex-col items-center gap-6">
+        <h2 className="font-raleway text-[20px] leading-[120%] font-bold">All your files in one secure location, accessible anywhere.</h2>
 
-        <p>
+        <p className="px-3.75">
           Fylo stores all your most important files in one secure location. Access them 
           wherever you need, share and collaborate with friends family, and co-workers.
         </p>
         
-        <button type="button">Get Started</button>
+        <button 
+          type="button" 
+          className="
+            px-20 py-4 rounded-3xl bg-linear-150 from-[#5CD7D7] to-[#34A0CD] to-70% 
+            text-[14px] leading-[120%] font-raleway font-bold
+          "
+        >
+          Get Started
+        </button>
       </div>
     </section>
   )
