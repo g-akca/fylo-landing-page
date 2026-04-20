451f2d47f39d0ac1f655f321b0778e5f2c600174
g-akca <gok.akca66@gmail.com>
2026-04-20 20:03:44 +0300

    Add Header styles, create Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import logoImg from "/images/logo.svg";
+
+import Navbar from "./Navbar";
+
 function Header() {
   return (
-    <header>
+    <header className="px-1.25 flex justify-between items-center">
+      <img src={logoImg} alt="Fylo logo" className="w-20" />
 
+      <Navbar />
     </header>
   )
 }
