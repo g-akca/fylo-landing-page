7d19c2e058ddb1b52cc4fa052fa67772481628ab
g-akca <gok.akca66@gmail.com>
2026-04-20 20:49:41 +0300

    Add HeroSection structure

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,21 @@
+import introImg from "/images/illustration-intro.png";
+
 function HeroSection() {
   return (
-    <>
-    </>
+    <section className="px-1.25 flex flex-col gap-18 items-center">
+      <img src={introImg} alt="Intro illustration" className="w-[95%]" />
+
+      <div className="text-center">
+        <h2>All your files in one secure location, accessible anywhere.</h2>
+
+        <p>
+          Fylo stores all your most important files in one secure location. Access them 
+          wherever you need, share and collaborate with friends family, and co-workers.
+        </p>
+        
+        <button type="button">Get Started</button>
+      </div>
+    </section>
   )
 }
 
