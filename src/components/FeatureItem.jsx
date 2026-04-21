9cb9a8126597a197566a3f6bbd807ab910e1cf88
g-akca <gok.akca66@gmail.com>
2026-04-21 23:23:37 +0300

    Add missing style

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,7 +1,7 @@
 function FeatureItem({ img, title, description }) {
   return (
     <div className="flex flex-col gap-6 items-center">
-      <img src={img} alt={title} className="" />
+      <img src={img} alt={title} className="w-6 aspect-square" />
 
       <div className="flex flex-col gap-2 text-center">
         <h2 className="font-raleway text-[20px] leading-[120%] font-bold">{title}</h2>
