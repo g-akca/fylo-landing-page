d29bbb81ded389b79d9571c009b311b117ba9cdb
g-akca <gok.akca66@gmail.com>
2026-04-21 22:15:23 +0300

    Install FontAwesome and add initial Footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
   return (
-    <footer>
+    <footer className="bg-navy-950 px-6 py-12 flex flex-col gap-12">
+      <div className="flex flex-col gap-14">
+        <div>
 
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+
+      <div className="flex justify-center gap-2.5">
+        <FontAwesomeIcon icon={faFacebookF} />
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faInstagram} />
+      </div>
     </footer>
   )
 }
