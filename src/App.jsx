3a5d14252b39d7eaad84e0f73dc33e34b7cab754
g-akca <gok.akca66@gmail.com>
2026-04-21 19:34:08 +0300

    Add App curvy background with color change

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import HeroSection from "./components/HeroSection";
 import MainSection from "./components/MainSection";
 import Footer from "./components/Footer";
 
+import curvyBgMobile from "/images/bg-curvy-mobile.svg";
+
 function App() {
   return (
-    <div className="text-base leading-base text-white min-h-screen bg-navy-850">
-      <div className="px-5 py-6 flex flex-col gap-12">
+    <div
+      className="
+        relative text-base leading-base text-white min-h-screen 
+        bg-linear-to-b from-navy-850 from-[420px] to-navy-900 to-[420px]
+      "
+    >
+      <img src={curvyBgMobile} className="absolute top-70" />
+
+      <div className="relative px-5 py-6 flex flex-col gap-12 z-10">
         <Header />
 
         <div className="flex flex-col gap-35">
