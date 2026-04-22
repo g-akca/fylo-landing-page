1ad4f27dbae333659d97a67a0c66e83adfa2ffc8
g-akca <gok.akca66@gmail.com>
2026-04-22 20:56:30 +0300

    Add tablet responsive Navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav>
-      <ul className="flex gap-6 font-raleway text-[12px] leading-[120%]">
+      <ul className="flex gap-6 tablet:gap-12 font-raleway text-[12px] tablet:text-[16px] leading-[120%]">
         <li><a href="#">Features</a></li>
         <li><a href="#">Team</a></li>
         <li><a href="#">Sign In</a></li>
