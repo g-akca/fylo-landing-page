31a17b07a52a0e357eabfdf9997d81b32b0c0abf
g-akca <gok.akca66@gmail.com>
2026-04-22 23:48:03 +0300

    Change Footer structure to adapt to tablet design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,30 +10,28 @@ import SocialMediaIcon from "./SocialMediaIcon";
 
 function Footer() {
   return (
-    <footer className="bg-navy-950 px-6 pt-65 pb-12 flex flex-col gap-12">
-      <div className="flex flex-col gap-14">
-        <div className="flex flex-col gap-10">
-          <img src={logoImg} alt="Fylo logo" className="h-7.75 self-start" />
+    <footer className="bg-navy-950 px-6 tablet:px-14 pt-65 tablet:pt-62 pb-12 tablet:pb-16">
+      <img src={logoImg} alt="Fylo logo" className="h-7.75 mb-10 tablet:mb-14" />
 
-          <div className="flex flex-col gap-4">
-            <div className="grid grid-cols-[24px_1fr] gap-4">
-              <img src={locationIcon} alt="Location icon" />
-              <p className="text-[16px] leading-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-            </div>
+      <div className="tablet:grid tablet:grid-cols-[380px_172px] tablet:gap-14 tablet:justify-between tablet:justify-items-start">
+        <div className="grid grid-cols-[24px_1fr] gap-4 mb-4">
+          <img src={locationIcon} alt="Location icon" />
+          <p className="text-[16px] leading-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
 
-            <div className="grid grid-cols-[24px_1fr] gap-4">
-              <img src={phoneIcon} alt="Phone icon" />
-              <p>+1-543-123-4567</p>
-            </div>
+        <div className="flex flex-col gap-4 mb-14">
+          <div className="grid grid-cols-[24px_1fr] gap-4">
+            <img src={phoneIcon} alt="Phone icon" />
+            <p>+1-543-123-4567</p>
+          </div>
 
-            <div className="grid grid-cols-[24px_1fr] gap-4 items-center">
-              <img src={emailIcon} alt="Email icon" />
-              <p>example@fylo.com</p>
-            </div>
+          <div className="grid grid-cols-[24px_1fr] gap-4 items-center">
+            <img src={emailIcon} alt="Email icon" />
+            <p>example@fylo.com</p>
           </div>
         </div>
 
-        <div className="flex flex-col gap-10 text-[16px] leading-base">
+        <div className="flex flex-col gap-10 text-[16px] leading-base mb-12">
           <ul className="flex flex-col gap-4">
             <li><a href="#">About Us</a></li>
             <li><a href="#">Jobs</a></li>
@@ -47,12 +45,12 @@ function Footer() {
             <li><a href="#">Privacy</a></li>
           </ul>
         </div>
-      </div>
 
-      <div className="flex justify-center gap-2.5">
-        <SocialMediaIcon icon={faFacebookF} />
-        <SocialMediaIcon icon={faTwitter} />
-        <SocialMediaIcon icon={faInstagram} />
+        <div className="flex justify-center gap-2.5">
+          <SocialMediaIcon icon={faFacebookF} />
+          <SocialMediaIcon icon={faTwitter} />
+          <SocialMediaIcon icon={faInstagram} />
+        </div>
       </div>
     </footer>
   )
