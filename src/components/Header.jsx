e02e8bdd95bd7bdc5ace1740b017a6eb3f449b1b
g-akca <gok.akca66@gmail.com>
2026-04-22 20:53:32 +0300

    Add tablet responsive Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <header className="px-1.25 flex justify-between items-center">
+    <header className="px-1.25 tablet:px-0 flex justify-between items-center">
       <h1 className="sr-only">Fylo</h1>
       
-      <img src={logoImg} alt="Fylo logo" className="w-20" />
+      <img src={logoImg} alt="Fylo logo" className="w-20 tablet:w-auto tablet:h-10" />
 
       <Navbar />
     </header>
