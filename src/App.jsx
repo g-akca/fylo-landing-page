f8515780aadb22df2b7eb425e4c98948d0a9e858
g-akca <gok.akca66@gmail.com>
2026-04-22 20:21:33 +0300

    Change bg image logic, make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,29 @@ import MainSection from "./components/MainSection";
 import Footer from "./components/Footer";
 
 import curvyBgMobile from "/images/bg-curvy-mobile.svg";
+import curvyBgDesktop from "/images/bg-curvy-desktop.svg";
 
 function App() {
   return (
     <div
       className="
         relative text-base leading-base text-white min-h-screen 
-        bg-linear-to-b from-navy-850 from-[420px] to-navy-900 to-[420px]
+        bg-linear-to-b from-navy-850 from-[408px] to-navy-900 to-[408px]
+        tablet:from-[1240px] tablet:to-[1240px]
       "
     >
-      <img src={curvyBgMobile} className="absolute top-70" />
+      <div className="absolute top-70 tablet:top-207.5 w-full h-35.25 tablet:h-112.25 overflow-hidden">
+        <picture>
+          <source
+            media="(min-width: 768px)"
+            srcSet={curvyBgDesktop}
+          />
+          <img
+            src={curvyBgMobile}
+            className="absolute top-0 right-0 w-93.75 tablet:left-1/2 tablet:-translate-x-1/2 tablet:w-360 desktop:right-0 desktop:left-auto desktop:translate-x-0 max-w-none"
+          />
+        </picture>
+      </div>
 
       <div className="relative px-5 py-6 flex flex-col gap-12 z-10">
         <Header />
