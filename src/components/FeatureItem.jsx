33449d13aabf656b67b96e54e2816118e6523419
g-akca <gok.akca66@gmail.com>
2026-04-22 22:50:22 +0300

    Add tablet responsive FeatureItem styles

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,7 +1,7 @@
 function FeatureItem({ img, title, description }) {
   return (
-    <div className="flex flex-col gap-6 items-center">
-      <img src={img} alt={title} />
+    <div className="flex flex-col gap-6 tablet:gap-4 items-center">
+      <img src={img} alt={title} className="tablet:h-26" />
 
       <div className="flex flex-col gap-2 text-center">
         <h2 className="font-raleway text-[20px] leading-[120%] font-bold">{title}</h2>
