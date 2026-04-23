3e9e99f81c374e483e08e998633e2186cf23b697
g-akca <gok.akca66@gmail.com>
2026-04-23 22:29:19 +0300

    Add email validation and error message

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 function CTASection() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  function validateEmail(value) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    if (!validateEmail(email)) {
+      setError("Whoops, make sure it's an email");
+    }
+    else {
+      setError("");
+    }
+  }
+
   return (
     <div className="px-6 py-10 tablet:px-10 tablet:py-12 desktop:px-20 w-full max-w-96 tablet:max-w-120 desktop:max-w-215.75 bg-navy-850 rounded-[9px] flex flex-col gap-8 text-center shadow-[5px_5px_8px_rgba(0,0,0,0.25)] absolute -bottom-50 tablet:-bottom-62 desktop:-bottom-55 left-1/2 -translate-x-1/2">
       <div className="flex flex-col gap-4 tablet:gap-6 desktop:gap-2">
@@ -6,11 +26,12 @@ function CTASection() {
         <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
       </div>
 
-      <form className="flex flex-col desktop:flex-row gap-6 tablet:gap-4">
+      <form className="relative flex flex-col desktop:flex-row gap-6 tablet:gap-4 desktop:gap-6">
         <input 
           type="email" 
-          id="email" 
           placeholder="email@example.com" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="
             h-12 bg-white px-8 rounded-3xl shadow-[1px_1px_3px_rgba(7,4,59,0.16)] 
             text-navy-950 text-[12px] leading-4 text-center tablet:text-start
@@ -19,8 +40,15 @@ function CTASection() {
           " 
         />
 
+        {error && (
+          <p className="absolute left-4 -bottom-5 text-red-500 text-[10px] leading-base tracking-[0.5px] font-bold">
+            {error}
+          </p>
+        )}
+
         <button 
           type="submit"
+          onClick={handleSubmit}
           className="
             px-18 desktop:px-6 py-4 rounded-3xl bg-linear-150 from-[#5CD7D7] to-[#34A0CD] to-80% 
             text-[14px] leading-[120%] font-raleway font-bold shadow-[2px_3px_6px_rgba(0,0,0,0.16)]
