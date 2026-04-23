185e560a1336ef83285beeb3d34950fcbf0de87e
g-akca <gok.akca66@gmail.com>
2026-04-23 21:49:02 +0300

    Add desktop responsive CTASection styles

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,12 +1,12 @@
 function CTASection() {
   return (
-    <div className="px-6 py-10 tablet:px-10 tablet:py-12 w-full max-w-96 tablet:max-w-120 bg-navy-850 rounded-[9px] flex flex-col gap-8 text-center shadow-[5px_5px_8px_rgba(0,0,0,0.25)] absolute -bottom-50 tablet:-bottom-62 left-1/2 -translate-x-1/2">
-      <div className="flex flex-col gap-4 tablet:gap-6">
-        <h2 className="font-raleway font-bold text-[20px] leading-[120%]">Get early access today</h2>
+    <div className="px-6 py-10 tablet:px-10 tablet:py-12 desktop:px-20 w-full max-w-96 tablet:max-w-120 desktop:max-w-215.75 bg-navy-850 rounded-[9px] flex flex-col gap-8 text-center shadow-[5px_5px_8px_rgba(0,0,0,0.25)] absolute -bottom-50 tablet:-bottom-62 desktop:-bottom-55 left-1/2 -translate-x-1/2">
+      <div className="flex flex-col gap-4 tablet:gap-6 desktop:gap-2">
+        <h2 className="font-raleway font-bold text-[20px] leading-[120%] desktop:text-[40px]">Get early access today</h2>
         <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
       </div>
 
-      <form className="flex flex-col gap-6 tablet:gap-4">
+      <form className="flex flex-col desktop:flex-row gap-6 tablet:gap-4">
         <input 
           type="email" 
           id="email" 
@@ -15,14 +15,16 @@ function CTASection() {
             h-12 bg-white px-8 rounded-3xl shadow-[1px_1px_3px_rgba(7,4,59,0.16)] 
             text-navy-950 text-[12px] leading-4 text-center tablet:text-start
             placeholder:text-gray-400 placeholder:text-base placeholder:leading-base
+            desktop:w-120
           " 
         />
 
         <button 
           type="submit"
           className="
-            px-18 py-4 rounded-3xl bg-linear-150 from-[#5CD7D7] to-[#34A0CD] to-80% 
+            px-18 desktop:px-6 py-4 rounded-3xl bg-linear-150 from-[#5CD7D7] to-[#34A0CD] to-80% 
             text-[14px] leading-[120%] font-raleway font-bold shadow-[2px_3px_6px_rgba(0,0,0,0.16)]
+            desktop:grow
           "
         >
           Get Started For Free
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -5,7 +5,7 @@ import CTASection from "./CTASection";
 
 function MainSection() {
   return (
-    <main className="flex flex-col gap-37.5 tablet:gap-20 desktop:gap-30 pb-81.5 tablet:pb-46 relative">
+    <main className="flex flex-col gap-37.5 tablet:gap-20 desktop:gap-30 pb-81.5 tablet:pb-46 desktop:pb-60 relative">
       <div className="px-1.25 tablet:px-0 flex flex-col gap-40 tablet:gap-30 desktop:gap-36.5">
         <FeaturesSection />
         <ProductiveSection />
